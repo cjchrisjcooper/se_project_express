c133fb8588c002af3a9c82945a6af2c1c3707c68
Chris Cooper <cjchrisjcooper@gmail.com>
2024-04-22 19:01:21 -0700

    Finished basic schema for models

---
+++ models/clothingItem.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  weather: { type: String, enum: ["hot", "warm", "cold"] },
+  imageUrl: {
+    type: String,
+    require: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid url",
+    },
+  },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true },
+  likes: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+  },
+  createdAt: {},
+});
+
+module.exports = mongoose.model("item", clothingItemSchema);
