0e34ae119e3a38300e6f65e9f1ebe6c66e9ca978
Chris Cooper <cjchrisjcooper@gmail.com>
2024-04-29 23:39:30 -0700

    Fix: fixed esLint error

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const ERROR_CODES = require("../utils/errors");
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
-const { REQUEST_CREATED } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
