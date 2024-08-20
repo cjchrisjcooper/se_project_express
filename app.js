d8867badce78580eeaea3166be8374701db69acc
Chris Cooper <cjchrisjcooper@gmail.com>
2024-08-20 13:15:41 -0700

    environmental variable

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const mainRouter = require("./routes/index");
+require("dotenv").config();
 
 const app = express();
 
